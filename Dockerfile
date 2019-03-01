48748bf34a831f4b5da11bcdaa562d4feb981130
Cecylia Bocovich <cohosh@torproject.org>
2019-03-01 10:42:57 -0500

    modified script to optionally build snowflake

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM golang:1.11
 RUN apt-get update && apt-get install -y git libx11-dev tor sudo
 RUN apt-get clean
 RUN useradd -ms /bin/bash snowflake
+RUN adduser snowflake sudo
+RUN echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 USER snowflake
 WORKDIR /go/src/
 COPY script.sh .
--- script.sh
+++ script.sh
@@ -1,15 +1,44 @@
+#!/bin/sh
+# POSIX
+
+build=0
+
+while :; do
+    case $1 in
+        -b|-\?|--build)
+            build=1
+            ;;
+        -?*)
+            printf 'Warning: Unknown option: %s\n' "$1" >&2
+            ;;
+        *) #default case: no more options
+            break
+    esac
+
+    shift
+done
+
+
 cd snowflake.git
 cd broker
-go get -d -v
-go build -v
+
+if [ "$build" -ne "0" ]; then
+    go get -d -v
+    go build -v
+fi
 nohup ./broker -addr ":8080" -disable-tls &
 
 cd ../proxy-go
-go get -d -v
-go build -v
+if [ "$build" -ne "0" ]; then
+    go get -d -v
+    go build -v
+fi
 nohup ./proxy-go -broker "http://localhost:8080" &
 
 cd ../client
-go get -d -v
-go build -v
+if [ "$build" -ne "0" ]; then
+    go get -d -v
+    go build -v
+fi
+
 tor -f torrc-localhost
